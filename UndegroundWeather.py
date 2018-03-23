2bbb78be71dd9818787b0fc31e9e8d955a554705
PashaSyr1 <pashasyr@hotmail.com>
2018-03-23 17:12:27 +0500

    Добавлено давление и влажность

--- UndegroundWeather.py
+++ UndegroundWeather.py
@@ -8,7 +8,11 @@ def get_temp_street():
     extractor = weather.Extract("0acd2cbb929b017c")
     [location,current] = extractor.features("RU/Tyumen",(('geolookup',''),('now','')))
     temp = current.temp_c
+    pleassure = round(float(current.data['pressure_mb'])*(760/1013.25),1)
+    relative_humidity = int(current.data['relative_humidity'].replace("%",''))
     aio = Client('e9666ef66a0149679734021420f5680b')
+    aio.send('pleassure', pleassure)
+    aio.send('relative_humidity', relative_humidity)
     aio.send('temp_on_street', temp)
     
 while True:
