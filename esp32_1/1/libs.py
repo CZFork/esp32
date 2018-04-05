7f6e2a162ea839af2a742890f23c8e06f5c0941c
PashaSyr1 <pashasyr@hotmail.com>
2018-04-05 14:09:54 +0500

    Изменен файл main.py - более стабильный вариант

--- esp32_1/1/libs.py
+++ esp32_1/1/libs.py
@@ -1,37 +1,37 @@
-import time #библиотека для прерываний 
-from machine import Pin, PWM #импортируем класс Пин и ШИМ
-from onewire import OneWire #импортируем класс OneWire
-from ds18x20 import DS18X20 #импортируем класс датчика ds18b20
-import network #импортируем класс подключения к сети вай-фай
-from mqtt import MQTTClient #для реализации Mqtt клиента
-#import traceback #трассировка ошибки - необходима для работы модуля логгирования
-import urequests #для http запросов - необходима для отправки данных в телеграмм
+import time # библиотека для прерываний
+from machine import Pin, PWM # импортируем класс Пин и ШИМ
+from onewire import OneWire # импортируем класс OneWire
+from ds18x20 import DS18X20 # импортируем класс датчика ds18b20
+import network # импортируем класс подключения к сети вай-фай
+from mqtt import MQTTClient # для реализации Mqtt клиента
 
 
-class Temperature():
-    #Класс для датчика температуры DS18X20
+class Temperature:
+    # Класс для датчика температуры DS18X20
     
     def __init__(self, port):
         """Иницилизация класса"""
         self.port = port
         self.addr = []
         self.get_addr()
+        self.dat = ''
+        self.temp = 0.0
         
     def get_addr(self):
         """Функция получения адреса датчика"""
         self.dat = DS18X20(OneWire(Pin(self.port, Pin.IN)))
         try_id = 1
-        while len(self.addr)<1:
+        while len(self.addr) < 1:
             try:
+                time.sleep(2)
                 self.addr = self.dat.scan()[0]
-                time.sleep_ms(750)
+                time.sleep_ms(2750)
                 try_id += 1
             except:
                 time.sleep_ms(750)
                 try_id += 1
             finally:
                 print("Попытка соединения с датчиком {0}".format(try_id))
-            
 
     def get_temp(self):
             self.dat.convert_temp()
@@ -40,10 +40,9 @@ class Temperature():
          
             return self.temp
 
-    
-    
-class WiFi():
-    #Класс для подлючения к сети вай-фай
+
+class WiFi:
+    # Класс для подлючения к сети вай-фай
     def __init__(self, ssid, password):
         self.ssid = ssid
         self.password = password
@@ -53,20 +52,19 @@ class WiFi():
             """Функция для подключения к вай-фай сети"""
             station = network.WLAN(network.STA_IF)
 
-            if station.isconnected() == True:
+            if station.isconnected() is True:
                 print("Соединение установлено")
                 return
 
             station.active(True)
             station.connect(self.ssid, self.password)
 
-            while station.isconnected() == False:
+            while station.isconnected() is False:
                 pass
 
-        
-        
-class ActiveBuzzer():
-    #Класс для работы с звукоизвлекателем
+
+class ActiveBuzzer:
+    # Класс для работы с звукоизвлекателем
     def __init__(self, port):
         self.port = port
         self.pin = Pin(port, Pin.OUT)
@@ -87,22 +85,24 @@ class ActiveBuzzer():
             time.sleep_ms(time_sleep_ms)
             
     def sub_cb(self, topic, msg):
-        #Для MQTT подписка
-        if msg ==b"Включить":
+        # Для MQTT подписка
+        if msg == b"On":
             self.on()
-        elif msg ==b"Выключить":
+        elif msg == b"Off":
             self.off()
-            
 
-class MQTT():
-    #класс для управления данными отправляемыми по протоколу mqtt
-    def __init__(self, client_id, server="io.adafruit.com", user="PashaSyr", password="e9666ef66a0149679734021420f5680b", port=1883):
+
+class MQTT:
+    # класс для управления данными отправляемыми по протоколу mqtt
+    def __init__(self, client_id, server="io.adafruit.com", user="PashaSyr",
+                 password="e9666ef66a0149679734021420f5680b", port=1883):
             self.client = MQTTClient(client_id, server=server, user=user, password=password, port=port)
         
     def pub(self, topic, message):
         self.client.connect()
         self.client.publish(str(topic), str(message))
         self.client.disconnect()
+
     def sub(self, topic):
         self.client.connect()
         self.client.subscribe(topic)
@@ -112,26 +112,27 @@ class MQTT():
         return result
         
         
-class Button():
-    #Класс для управление кнопкой
-    
+class Button:
+    # Класс для управление кнопкой
     def __init__(self, port):
         self.port = port
         self.pin = Pin(port, Pin.IN, Pin.PULL_UP)
         self.mode = 0
-        
-            
+
     def but_pressed(self):
         self.mode = self.pin.value()
         return self.mode
         
 
-class RGBLed():
-    #Класс для управления светодиодом
+class RGBLed:
+    # Класс для управления светодиодом
     def __init__(self, pin_r, pin_g, pin_b):
         self.pin_r = PWM(Pin(pin_r))
         self.pin_g = PWM(Pin(pin_g))
         self.pin_b = PWM(Pin(pin_b))
+        self.r = 0
+        self.g = 0
+        self.b = 0
         self.set_rgb(0, 0, 0)
 
     def set_rgb(self, r, g, b):
@@ -145,7 +146,7 @@ class RGBLed():
         self.pin_g.duty(self.duty_translate(self.g))
         self.pin_b.duty(self.duty_translate(self.b))
 
-    def duty_translate(self, n):
+    @staticmethod
+    def duty_translate(n):
         """translate values from 0-255 to 0-1023"""
         return int((float(n) / 255) * 1023)
-        
--- esp32_1/1/main.py
+++ esp32_1/1/main.py
@@ -3,61 +3,93 @@ import time
 import urequests
 import machine
 
-def cb(topic, msg):
-    print(topic, msg)
-    return topic, msg
-
-buz = ActiveBuzzer(17) #подключение пищалки
-but = Button(15) #подключение кнопки
-wifi = WiFi("WiFi-DOM.ru-6285", "Nastya26042015") #подключение вай фай
-temp = Temperature(2) #подключение датчика температуры
+# подключение пищалки
+buz = ActiveBuzzer(17)
+# подключение кнопки
+but = Button(15)
+# подключение вай фай
+wifi = WiFi("WiFi-DOM.ru-6285", "Nastya26042015")
+# подключение датчика температуры
+temp = Temperature(2)
+# подключение светодиода
 rgb = RGBLed(pin_r, pin_g, pin_b)
 
-
-
-#Иницилизация клиента
+# Иницилизация клиента
 mqtt = MQTT(client_id="1")
 mqtt.client.set_callback(cb)
-topic, msg = mqtt.sub('PashaSyr/feeds/break_time') #стандартное время без включания будильника
+# стандартное время без включания будильника
+topic, msg = mqtt.sub('PashaSyr/feeds/break_time')
 break_time = int(msg)
-topic, msg = mqtt.sub('PashaSyr/feeds/target_temp') #пороговая температура для включения будильника
+# пороговая температура для включения будильника
+topic, msg = mqtt.sub('PashaSyr/feeds/target_temp')
 target_temp = int(msg)
 
 while True:
     try:
-        temp = round(temp.get_temp(),1) #проверка температуры
-        mqtt.pub('PashaSyr/feeds/temp', str(temp)) #публикация температуры
-        if temp <= target_temp and break_time == 0: #если температура меньше целевой и время без будильника истекло
-            topic, msg = mqtt.sub('PashaSyr/feeds/break_time') #стандартное время без включания будильника
+        # проверка температуры
+        temp = round(temp.get_temp(), 1)
+        # публикация температуры
+        mqtt.pub('PashaSyr/feeds/temp', str(temp))
+        # если температура меньше целевой и время без будильника истекло
+        if temp <= target_temp and break_time == 0:
+            # стандартное время без включания будильника
+            topic, msg = mqtt.sub('PashaSyr/feeds/break_time')
             break_time = int(msg)
-            print("Beep on") #включаем будильник
-            mqtt.pub('PashaSyr/feeds/beep', "Включить") #запускаем будильник
-            get_temp_time = 110 #переменная для проверки температуры при включенном будильнике 
-            while True: #бесконечный цикл
-                if get_temp_time == 0: #если время проверки температуры истекло
-                    temp = round(temp.get_temp(),1) #проверяем температуру
-                    mqtt.pub('PashaSyr/feeds/temp', str(temp)) #публикация температуры
-                    get_temp_time = 110 #устанавливаем время в начальное состояние
-                    if temp >  target_temp: #если температура за это время повысилась выше целевой, 
-                        mqtt.pub('PashaSyr/feeds/beep', "Выключить") #запускаем будильник #отправляем данные на сервер
+            # включаем будильник
+            print("Beep on")
+            # запускаем будильник
+            mqtt.pub('PashaSyr/feeds/beep', "Включить")
+            # переменная для проверки температуры при включенном будильнике
+            get_temp_time = 110
+            # бесконечный цикл
+            while True:
+                # если время проверки температуры истекло
+                if get_temp_time == 0:
+                    # проверяем температуру
+                    temp = round(temp.get_temp(),1)
+                    # публикация температуры
+                    mqtt.pub('PashaSyr/feeds/temp', str(temp))
+                    # устанавливаем время в начальное состояние
+                    get_temp_time = 110
+                    # если температура за это время повысилась выше целевой,
+                    if temp > target_temp:
+                        # выключаем будильник
+                        print("Beep off")
+                        # отправляем данные на сервер
+                        mqtt.pub('PashaSyr/feeds/beep', "Выключить")
                         break
-                else: #если время проверки не истекло
-                    topic, msg = mqtt.sub('PashaSyr/feeds/beep') #пороговая температура для включения будильника
+                else:
+                    # если время проверки не истекло
+                    # проверка - выключен ли будильник на сайте
+                    topic, msg = mqtt.sub('PashaSyr/feeds/beep')
                     data = str(msg)
-                    if data == "Выключить": #если он выключен
-                        print("Beep off") #выключаем будильник
+                    if data == "Выключить":
+                        # если он выключен
+                        # выключаем будильник
+                        print("Beep off")
                         break
-                    else:    #если будильник не выключили
-                        get_temp_time -= 1  #отнимаем один из времени проверки 
-                        time.sleep(0.5) #засыпаем на полсекунды
-        elif temp <= target_temp and break_time>0: #если температура меньше или равна целевой а время без включения не истекло
-            break_time -= 1 #уменьшаем время без будильника на 1
-        time.sleep(58.5)
+                    else:
+                        # если будильник не выключили
+                        # отнимаем один из времени проверки
+                        get_temp_time -= 1
+                        # засыпаем на полсекунды
+                        time.sleep(0.5)
+        elif temp <= target_temp and break_time > 0:
+            # если температура меньше или равна целевой а время без включения не истекло
+            # уменьшаем время без будильника на 1
+            break_time -= 1
+        time.sleep(298.5)
         
     except Exception as err:
-        try:
-            req = urequests.post('https://api.telegram.org/bot399910911:AAEh2Wv3YWKe1XSOW0hXeBmaJfbp1O4VBlk/sendMessage?chat_id=24504089&text={0}'.format(err))
-            machine.reset()
-        except:
-            time.sleep(300)
-            machine.reset()
+        if Exception == 'CRC error':
+            time.sleep(1)
+        elif Exception == 'bytes index out of range':
+            time.sleep(1)
+        else:
+            try:
+                req = urequests.post(
+                    """https://api.telegram.org/bot399910911:AAEh2Wv3YWKe1XSOW0hXeBmaJfbp1O4VBlk/
+                    sendMessage?chat_id=24504089&text={0}""".format(err))
+                machine.reset()
+            except Exception as err:
+                machine.reset()
