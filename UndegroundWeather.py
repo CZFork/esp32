077dd4c36f123b3321c088136780b3dcfb448e1b
SyrnikovPavel <SyrnikovPavel@gmail.com>
2018-04-05 13:40:34 +0500

    Изменен опрос внешних датчиков

--- UndegroundWeather.py
+++ UndegroundWeather.py
@@ -1,27 +1,45 @@
 # coding: utf-8
 
-from WunderWeather import weather
+import requests
 from Adafruit_IO import *
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import time
 
+broker = '127.0.0.1'
+state_topic = 'home-assistant/temp/temp_on_street'
+client = mqtt.Client("ha-client")
+client.connect(broker)
+client.loop_start()
+
+
 def get_temp_street():
-    extractor = weather.Extract("0acd2cbb929b017c")
-    [location,current] = extractor.features("RU/Tyumen",(('geolookup',''),('now','')))
-    temp = current.temp_c
-    pleassure = round(float(current.data['pressure_mb'])*(760/1013.25),1)
-    relative_humidity = int(current.data['relative_humidity'].replace("%",''))
+    url = 'http://narodmon.ru/api/sensorsOnDevice?id=1405&uuid=841199ada28c8747a112ad7c8d90a202&api_key=fGUwrZDQQaFan&lang=ru'
+    res=requests.get(url)
+    data = res.json()
+    temp = data['sensors'][0]['value'] #температура
+    pleassure = data['sensors'][1]['value'] #давление
+    lux = data['sensors'][2]['value'] #освещенность
+    url = 'http://narodmon.ru/api/sensorsOnDevice?id=2401&uuid=841199ada28c8747a112ad7c8d90a202&api_key=fGUwrZDQQaFan&lang=ru'
+    res=requests.get(url)
+    data = res.json()
+    relative_humidity = data['sensors'][2]['value']
     aio = Client('e9666ef66a0149679734021420f5680b')
     aio.send('pleassure', pleassure)
     aio.send('relative_humidity', relative_humidity)
     aio.send('temp_on_street', temp)
+    aio.send('lux', lux)
+    return temp
 
 def main():
     while True:
         try:
-            get_temp_street()
-            time.sleep(60)
-        except:
+            temp = get_temp_street()
+            client.publish(state_topic, temp)
             time.sleep(300)
+        except Exception as er:
+            print(er)
+            time.sleep(30)
 
 if __name__=="__main__":
     main()
